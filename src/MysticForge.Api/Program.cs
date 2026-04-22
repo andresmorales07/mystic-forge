bd250bb9d9f3f800c8f81afcdde0b1f2d8f89581
Andres <30600192+andresmorales07@users.noreply.github.com>
2026-04-22 19:39:17 -0400

    feat: configure Serilog with console + rolling file sinks

--- src/MysticForge.Api/Program.cs
+++ src/MysticForge.Api/Program.cs
@@ -1,11 +1,18 @@
 using Hangfire;
 using MysticForge.Infrastructure;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services));
 
 builder.Services.AddMysticForgeInfrastructure(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 app.MapGet("/", () => "Mystic Forge API");
 
